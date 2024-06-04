ee37f541c44f67322d43d7e9bfa84483b3bf5786
CodeCaptianess <lmancbrayilova22@gmail.com>
2024-06-04 13:04:40 +0400

    Realestate-website

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/** Change background header */
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
