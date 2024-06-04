f9953bad8bdcbe5da96f36f948123315b9c39c32
CodeCaptianess <lmancbrayilova22@gmail.com>
2024-06-04 23:29:58 +0400

    Realestate-website

--- js/script.js
+++ js/script.js
@@ -92,3 +92,11 @@ if (selectedTheme) {
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
     themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
 }
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
