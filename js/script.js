003c31b618efad520e7ab9479cc64fcce7c53503
CodeCaptianess <lmancbrayilova22@gmail.com>
2024-06-04 17:23:48 +0400

    Realestate-website

--- js/script.js
+++ js/script.js
@@ -58,5 +58,12 @@ function scrollActive(){
         const sectionHeight = current.offsetHeight,
               sectionTop = current.offsetTop - 58,
               sectionId = current.getAttribute('id')
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }  
     })
 }
+
+window.addEventListener('scroll', scrollActive)
