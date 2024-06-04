7dc453e7666ab6a9cd6b3226291d6f15081db3e5
CodeCaptianess <lmancbrayilova22@gmail.com>
2024-06-04 15:44:02 +0400

    Realestate-website

--- js/script.js
+++ js/script.js
@@ -12,9 +12,27 @@ var swiperPopular = new Swiper(".popular__container", {
     centeredSlides: true,
     slidesPerView: 'auto',
     loop: true,
-     
+
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
 });
+
+/** Value accordion */
+const accordionItems = document.querySelectorAll('.value__accordion-item');
+
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value__accordion-header')
+
+    accordionHeader.addEventListener('click', () =>{
+        toogleItem(item)
+    })
+})
+
+const toogleItem = (item) =>{
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+}
