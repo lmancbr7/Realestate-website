01b3dae9555cf2b86e3059c8641c160ddc25ca48
CodeCaptianess <lmancbrayilova22@gmail.com>
2024-06-05 16:27:02 +0400

    Realestate-website

--- js/script.js
+++ js/script.js
@@ -107,7 +107,7 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2500,
     delay: 400,
-    reset: true
+    // reset: true
 })
 
 sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`)
